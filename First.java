30db3cc018a4cd4a2aeaf8e4d7fd7ed597f49ee5
spasevska <spasevska.dijana@gmail.com>
2021-06-06 23:02:21 +0200

    update 06.06.2021

--- First.java
+++ First.java
@@ -3,35 +3,15 @@ package NameFirst;
 import java.util.Scanner;
 
 public class First {
-		public static void main(String[] args) {
-			System.out.println("Factorial of 5 is: " + factorial(5));
-		}
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Write the temperature in Celsuis: ");
+		float C = scan.nextFloat();
 		
-		static int factorial (int n) {
-			if (n <= 1) {
-				return n;
-			} else {
-				return factorial(n-1) + factorial(n-2);
-			}
-		}
-
-// =====================================
-	
-//	public static int fib (int n) {
-//
-//		if (n <= 1)
-//			return n;
-//		else
-//			return fib(n-1) + fib(n-2);
-//	}
-//
-//	public static void main(String[] args) {
-//		int n = 9;
-//		if(n < 0)
-//			System.out.println("fibonnacci number is not defined	");
-//		else
-//			System.out.println(fib(n));
-//
-//	}
-
+		float F = C * (9/5) + 32;
+		System.out.println(C + " Celsius is " + F + " Fahrenheit");
+	}
 }
+
